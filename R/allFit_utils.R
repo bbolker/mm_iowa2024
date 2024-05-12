645cff0efc64e2120f805cc33dc7a4d3c451adff
Ben Bolker <bbolker@gmail.com>
2024-05-12 17:26:01 -0400

    working

--- R/allFit_utils.R
+++ R/allFit_utils.R
@@ -4,7 +4,12 @@ glance_allfit_NLL <- function(aa) {
     glance(aa) |> dplyr::select(optimizer, NLL_rel) |> dplyr::arrange(NLL_rel)
 }
 
-plot_allfit <- function(aa, keep_effects = "fixed", dodgewidth = 0.5, sep_corsd = "ran_pars" %in% keep_effects) {
+#' @param aa an object of class `allFit`
+#' @param keep_effects which effects to keep?
+#' @param dodgewidth how far apart to space point+range elements
+#' @param sep_corsd whether to separate correlation and SD terms into separate facets
+plot_allfit <- function(aa, keep_effects = "fixed", dodgewidth = 0.5,
+                        sep_corsd = "ran_pars" %in% keep_effects) {
     gg <- glance_allfit_NLL(aa)
     tt <- tidy(aa) |> mutate(across(optimizer, \(x) factor(x, levels = rev(gg$optimizer),
                                                            labels = rev(sprintf("%s (%1.2g)", gg$optimizer, gg$NLL_rel)))))
@@ -13,7 +18,8 @@ plot_allfit <- function(aa, keep_effects = "fixed", dodgewidth = 0.5, sep_corsd 
     gg1 <- ggplot(tt, aes(y = term, x = estimate, colour = optimizer)) +
         geom_pointrange(aes(xmin=estimate-2*std.error, xmax = estimate + 2*std.error),
                     position = pd) +
-        scale_color_discrete_sequential(guide = guide_legend(reverse = TRUE))
+        scale_color_discrete_sequential(guide = guide_legend(reverse = TRUE),
+                                        name = "optimizer (ΔNLL)")
     if (sep_corsd) {
         gg1 <- gg1 + facet_wrap(~startsWith(term, "cor"), labeller=label_both, scale = "free")
     }
